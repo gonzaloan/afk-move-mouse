7eab5f67e9a50ada309b4a6feae85782793859f1
gonzalo.munoz <gonzaloan.munoz@gmail.com}>
2023-01-03 14:30:29 -0300

    change script to work in company laptops :P

--- main.py
+++ main.py
@@ -1,27 +1,14 @@
-import os
-import ctypes
+import random
+import pyautogui
 import time
-
-def afk_me():
-    if os.name == 'nt':
-        ES_CONTINUOUS = 0x80000000
-        ES_SYSTEM_REQUIRED = 0x00000001
-        ctypes.windll.kernel32.SetThreadExecutionState(ES_CONTINUOUS | ES_SYSTEM_REQUIRED)
-    elif os.name == 'posix':
-        try:
-            # Trying to use caffeinate in Mac
-            os.system("caffeinate -disu &")
-        except:
-            try:
-                # if not, we use linux
-                os.system("xset s off")
-                os.system("xset -dpms")
-                os.system("xset s noblank")
-            except:
-                print("Could not prevent the system from going to sleep. I suck! :( ")
-    else:
-        print("shit happened, sorry, dude :( ")
+import mouse
 
 while True:
-    afk_me()
-    time.sleep(60)
+	x = random.randint(600,700)
+	y = random.randint(200,600)
+	pyautogui.moveTo(x,y,0.3)
+	pyautogui.FAILSAFE = False
+	time.sleep(10)
+	mouse.click()
+
+
