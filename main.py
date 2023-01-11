615b688f964eaa1a0b7db03da3f19e483ccf8449
gonzalo.munoz <gonzaloan.munoz@gmail.com}>
2023-01-11 17:19:02 -0300

    change script to work with other command

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
-import pyautogui
+import os
+import ctypes
 import time
 
+def afk_me():
+    if os.name == 'nt':
+        ES_CONTINUOUS = 0x80000000
+        ES_SYSTEM_REQUIRED = 0x00000001
+        ctypes.windll.kernel32.SetThreadExecutionState(ES_CONTINUOUS | ES_SYSTEM_REQUIRED)
+    elif os.name == 'posix':
+        try:
+            # Trying to use caffeinate in Mac
+            os.system("caffeinate -disu &")
+        except:
+            try:
+                # if not, we use linux
+                os.system("xset s off")
+                os.system("xset -dpms")
+                os.system("xset s noblank")
+            except:
+                print("Could not prevent the system from going to sleep. I suck! :( ")
+    else:
+        print("shit happened, sorry, dude :( ")
+
 while True:
-    pyautogui.moveRel(10, 0, duration=0.1)
-    pyautogui.moveRel(0, 10, duration=0.1)
-    pyautogui.moveRel(-10, 0, duration=0.1)
-    pyautogui.moveRel(0, -10, duration=0.1)
-    time.sleep(2)
+    afk_me()
+    time.sleep(60)
